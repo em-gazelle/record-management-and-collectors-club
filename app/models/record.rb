39940da309df663b3d257a5208d82fae862fcfc9
em-gazelle <emmaf9@gmail.com>
2018-04-15 23:38:50 -0400

    Method for analyzing artist record data

--- app/models/record.rb
+++ app/models/record.rb
@@ -5,4 +5,41 @@ class Record < ApplicationRecord
 	# validates_presence :album_title#, uniqueness: true
 	# validates :artist#, uniqueness: true
 	validates :year, numericality: true
+
+	def self.artist_analysis
+		@hash_of_artists_hash_of_stats = {}
+		puts self
+
+		Record.all.each do |record|
+			@hash_of_artists_hash_of_stats[record.artist.to_sym] ||= {total_records_owned: 0, record_range: [], album_title_mode: {} }
+
+			@hash_of_artists_hash_of_stats[record.artist.to_sym][:total_records_owned] = @hash_of_artists_hash_of_stats[record.artist.to_sym][:total_records_owned] + 1
+
+			year_range = @hash_of_artists_hash_of_stats[record.artist.to_sym][:record_range]
+			
+			if year_range == []
+				@hash_of_artists_hash_of_stats[record.artist.to_sym][:record_range] = [record.year, record.year]
+			elsif record.year < year_range.first
+				@hash_of_artists_hash_of_stats[record.artist.to_sym][:record_range] = [record.year, year_range[1]]
+			elsif record.year > year_range.last
+				@hash_of_artists_hash_of_stats[record.artist.to_sym][:record_range] = [year_range[0], record.year]
+			end
+
+			# @artist_title_mode = @hash_of_artists_hash_of_stats[record.artist.to_sym][:album_title_mode]
+			
+			record_title_words = record.album_title.scan(/[a-zA-z]|\'|\s/).join.downcase.split(" ")
+
+			record_title_words.each do |word|
+				@hash_of_artists_hash_of_stats[record.artist.to_sym][:album_title_mode][word.to_sym] = (@hash_of_artists_hash_of_stats[record.artist.to_sym][:album_title_mode][word.to_sym] || 0) + 1
+			end
+		end
+		
+		@hash_of_artists_hash_of_stats.each do |artist, artist_stats_hash|
+			@hash_of_artists_hash_of_stats[artist.to_sym][:album_title_mode] = artist_stats_hash[:album_title_mode].key(artist_stats_hash[:album_title_mode].values.max).to_s
+			@hash_of_artists_hash_of_stats[artist.to_sym][:record_range] = @hash_of_artists_hash_of_stats[artist.to_sym][:record_range].join(" - ")
+		end
+
+		@hash_of_artists_hash_of_stats
+	end
+
 end
--- spec/models/record_spec.rb
+++ spec/models/record_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Record, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	let(:record1) { Fabricate(:record, year: 1999, artist: "The Beatles", album_title: "Help!") }
+	let(:record2) { Fabricate(:record, year: 2018, artist: "The Beatles", album_title: "Please please me") }
+	let(:record3) { Fabricate(:record, year: 1989, artist: "The Beatles", album_title: "Let It Be - please?! help!") }
+	let(:record4) { Fabricate(:record, year: 1988, artist: "Elvis", album_title: "Elvis for Everyone") }
+
+	describe 'self.artist_analysis' do
+		context 'when records exist' do
+			it 'returns hash of artist stats' do
+				[1,2,3,4].map{|n| eval("record#{n}")}
+
+				expect(Record.artist_analysis).to eq({
+					"The Beatles".to_sym => {total_records_owned: 3, record_range: "1989 - 2018", album_title_mode: "please" }, 
+					"Elvis".to_sym => {total_records_owned: 1, record_range: "1988 - 1988", album_title_mode: "elvis" }
+					})
+			end
+		end
+		context 'when no records' do
+			it 'returns empty hash' do
+				# expect(Record.artist_analysis).to eq({total_records_owned: 0, record_range: [], album_title_mode: {} })
+				expect(Record.artist_analysis).to eq({})
+			end
+		end
+	end
+
 end
